dc0e271728c5c6b8fad8b21744dfee61b0966d52
Tomoloju Victor <tomoloju45@gmail.com>
2019-12-29 08:19:54 -1100

    Update script.js

--- script.js
+++ script.js
@@ -1,172 +1,51 @@
-
-function createElement(type,className){
-  var element = document.createElement(type);
-  if(className){
-    element.classList.add(className);
-  }
-  return element;
-}
-
-
-function createParagraph(text, className){
-
-  var p = createElement("p",className);
-  p.innerText = text;
-
-  return p;
-}
-
-///createParagraph("No TODOS to display", "no-todos");
-
-function createUl(className){
-  var ul = createElement("ul",className);
-  return  ul;
-}
-
-function createDiv(className){
-  var div  = createElement("div",className);
-return div;
-}
-
-function createbutton(text, className, dataPurpose){
-  var btn  = createElement("button",className);
-  btn.innerText = text;
-  btn.setAttribute("data-purpose", dataPurpose);
-return btn;
-}
-
-function createTODO(text)
-{
-  var li = createElement("li", "todo");
-  var p = createParagraph(text);
-  li.append(p);
-  var buttoncontainer = createDiv("buttons");
-  var upbtn = createbutton("Up", "up","up");
-  var downbtn = createbutton("DOWN", "down","down");
-  var removebtn = createbutton("Remove", "remove","remove");
-
-  buttoncontainer.append(upbtn);
-  buttoncontainer.append(downbtn);
-  buttoncontainer.append(removebtn);
-
-  li.append(buttoncontainer);
-
-  return li;
-}
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>TODO LIST</title>
+  <link rel="stylesheet" href="styles.css">
+</head>
+<body>
 
 
-var todoinput = document.getElementById("todo-input");
+  <div class="todo-container">
 
-var add_btn = document.getElementById("add-todo");
+    <header id="todo-header">
+      <h1>TODOS</h1>
+<input type="text" placeholder="Enter a task" id="todo-input">
 
-var maincontainer = document.getElementById("todo-main");
-
-
-add_btn.addEventListener("click",function(){
-
-  if(todoinput.value.length>0)
-    {
-      todo.style.opacity = 0;
-      setTimeout(function(){
-        todo.style.opacity=1;
-      },100);
-
-  var todo = createTODO(todoinput.value);
-
-  if(!maincontainer.querySelector(".todo"))
-  {
-    var noTodosp = document.querySelector("p.no-todos");
-    maincontainer.removeChild(noTodosp);
-    
-  var ul = createUl("todo-list");
-  ul.append(todo);
-  maincontainer.append(ul);
-  }
- 
-  else{
-    var ul = document.querySelector(".todo-list");
-    ul.append(todo);
-  }
-
-  todoinput.value = "";
-    } ///
-
-})
-
-todoinput.addEventListener("keyup", function(e){
-
-    if(todoinput.value.length>0)
-    {
-       if(e.keyCode === 13){
-  var todo = createTODO(todoinput.value);
-
-  todo.style.opacity = 0;
-    setTimeout(function(){
-      todo.style.opacity=1;
-    },100);
-
-  if(!maincontainer.querySelector(".todo"))
-  {
+<button id="add-todo">Add</button>
+    </header>
   
 
-    var noTodosp = document.querySelector("p.no-todos");
-    maincontainer.removeChild(noTodosp);
-  var ul = createUl("todo-list");
-  ul.append(todo);
-  maincontainer.append(ul);
-  }
- 
-  else{
-    var ul = document.querySelector(".todo-list");
-    ul.append(todo);
-  }
+  <main id="todo-main">
 
-  todoinput.value = "";
-  } ///
-    } ///
- 
-});
+ <p class="no-todos">
+          No TODOS to display
+      </p>
 
+     
+      
 
-maincontainer.addEventListener("click", function(e){
+        <!--<ul class="todo-list"> 
+          
+          <li class="todo">
+          <p>Tasks 1</p>
 
-  if(e.target.nodeName == "BUTTON"){
-   var button = e.target;
-   var typeButoon = button.getAttribute("data-purpose");
+          <div class="button">
+            <button class="up">UP</button>
+            <button class="down">Down</button>
+            <button class="remove">Remove</button>
+          </div>
+        </li>  </ul>-->
 
-   var li = button.parentElement.parentElement;
-   var ul = li.parentElement;
+     
+  </main>
 
-   switch(typeButoon){
+</div>
 
-    case "remove":
-      ul.removeChild(li);
-     if(ul.children.length ===0){
-        var p = createParagraph("No TODOs to display", "no-todos");
-        var ul = document.querySelector(".todo-list");
-        maincontainer.removeChild(ul);
-        maincontainer.append(p);
-      }
-      break;
-
-      case "up":
-        var previousElement = li.previousElementSibling;
-        if(previousElement!==null){
-          ul.removeChild(li);
-          ul.insertBefore(li,previousElement);
-        }
-      break;
-
-      case "down":
-        var nextElement  = li.nextElementSibling;
-        if(nextElement!==null){
-          ul.removeChild(li);
-          ul.insertBefore(li,nextElement.nextElementSibling);
-        }
-
-   } ///
-  }
-  
-})
-
-
+  <script src="script.js"></script>
+</body>
+</html>
